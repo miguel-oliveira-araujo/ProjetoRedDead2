5a4f71db05cabca9bec829f19d909631bd5912dd
miguel-oliveira-araujo <migueloliveira.araujo@etec.sp.gov.br>
2023-03-21 22:07:18 -0300

    js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ function mudar(){
     let trocar = Math.trunc(Math.random()*10);
     img.src = "img/" + trocar +".jpg";
 }
+
+function trocar(){
+    let img3 = document.getElementById("img3");
+    let trocar = Math.trunc(Math.random()*2);
+    img.src = "img3/foto" + trocar + ".jpg";
+}
